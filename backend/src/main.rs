f38764069a1ab0df9a430f5034bbef850ef36261
Jonathan Lavigne <jlavigne@techso.com>
2020-03-30 19:30:29 -0400

    Added the dockerization of the backend

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -9,7 +9,7 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .configure(greet::configure)
     })
-        .bind("127.0.0.1:8000")?
+        .bind(":8000")?
         .run()
         .await
 }
